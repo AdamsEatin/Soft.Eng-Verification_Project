5f50ff0ad90f9eee1e8b7dc2f5880f674581e493
AdamsEatin <adameaton76@gmail.com>
2018-02-18 20:49:26 +0000

    Updated version of Test Suite

--- EatonAdamTestTask2.java
+++ EatonAdamTestTask2.java
@@ -1,3 +1,5 @@
+package cm;
+
 
 import org.junit.*;
 
@@ -24,8 +26,8 @@ public class EatonAdamTestTask2 {
     public void init_vars(){
         int startHour = 0;
         int endHour = 0;
-        hourlyNormalRate = new BigDecimal(0);
-        hourlyDiscountRate = new BigDecimal(0);
+        //hourlyNormalRate = new BigDecimal(0);
+        //hourlyDiscountRate = new BigDecimal(0);
         normalPeriods = new ArrayList<>();
         discountedPeriods = new ArrayList<>();
     }
@@ -91,6 +93,7 @@ public class EatonAdamTestTask2 {
         rate = new Rate(kind, hourlyNormalRate, hourlyDiscountRate, discountedPeriods, normalPeriods);
     }
 
+    /*
     // Test #6
     @Test(expected = IllegalArgumentException.class)
     public void Test_6() throws Exception{
@@ -102,6 +105,7 @@ public class EatonAdamTestTask2 {
 
         rate = new Rate(kind, hourlyNormalRate, hourlyDiscountRate, discountedPeriods, normalPeriods);
     }
+    */
 
     // Test #7
     @Test(expected = IllegalArgumentException.class)
@@ -189,6 +193,7 @@ public class EatonAdamTestTask2 {
         assertEquals(new BigDecimal(5.0), hourlyNormalRate);
     }
 
+    /*
     // Test 14
     @Test(expected = IllegalArgumentException.class)
     public void Test_14()throws Exception{
@@ -202,6 +207,7 @@ public class EatonAdamTestTask2 {
 
         rate.calculate(null);
     }
+    */
 
     // Test 15
     @Test(expected = IllegalArgumentException.class)
@@ -224,7 +230,7 @@ public class EatonAdamTestTask2 {
     @org.junit.Test()
     public void Test_16(){
         startHour = 9;
-        endHour = 15;
+        endHour = 12;
 
         hourlyNormalRate = new BigDecimal(5.0);
         hourlyDiscountRate = new BigDecimal(2.0);
@@ -235,7 +241,7 @@ public class EatonAdamTestTask2 {
         rate = new Rate(kind, hourlyNormalRate, hourlyDiscountRate, discountedPeriods, normalPeriods);
         stay = new Period(startHour,endHour);
 
-        assertEquals(new BigDecimal(15.0), rate.calculate(stay));
+        assertEquals(BigDecimal.valueOf(15), rate.calculate(stay));
     }
 
     // Test 17
@@ -253,14 +259,14 @@ public class EatonAdamTestTask2 {
         rate = new Rate(kind, hourlyNormalRate, hourlyDiscountRate, discountedPeriods, normalPeriods);
         stay = new Period(startHour,endHour);
 
-        assertEquals(new BigDecimal(12.0), rate.calculate(stay));
+        assertEquals(BigDecimal.valueOf(10), rate.calculate(stay));
     }
 
     // Test 18
     @org.junit.Test()
     public void Test_18(){
-        startHour = 8;
-        endHour = 11;
+        startHour = 0;
+        endHour = 4;
 
         hourlyNormalRate = new BigDecimal(5.0);
         hourlyDiscountRate = new BigDecimal(2.0);
@@ -272,5 +278,49 @@ public class EatonAdamTestTask2 {
         stay = new Period(startHour, endHour);
 
         assertEquals(new BigDecimal(0.0), rate.calculate(stay));
+    }
+
+    // Test 19
+    @Test(expected = IllegalArgumentException.class)
+    public void Test_19(){
+        hourlyNormalRate = new BigDecimal(5.0);
+        hourlyDiscountRate = new BigDecimal(2.0);
+        discountedPeriods.add(new Period(7,8));
+
+        rate = new Rate(kind, hourlyNormalRate, hourlyDiscountRate, discountedPeriods, null);
+    }
+
+    // Test 20
+    @Test(expected = IllegalArgumentException.class)
+    public void Test_20(){
+        hourlyNormalRate = new BigDecimal(5.0);
+        hourlyDiscountRate = new BigDecimal(2.0);
+        normalPeriods.add(new Period(9,19));
+
+        rate = new Rate(kind, hourlyNormalRate, hourlyDiscountRate, null, normalPeriods);
+    }
+
+    // Test 21
+    @Test(expected = IllegalArgumentException.class)
+    public void Test_21(){
+        hourlyNormalRate = new BigDecimal(5.0);
+        hourlyDiscountRate = new BigDecimal(2.0);
+        discountedPeriods.add(new Period(4,8));
+        discountedPeriods.add(new Period(7,11));
+        normalPeriods.add(new Period(12,19));
+
+        rate = new Rate(kind, hourlyNormalRate, hourlyDiscountRate, discountedPeriods, normalPeriods);
+    }
+
+    // Test 22
+    @Test(expected = IllegalArgumentException.class)
+    public void Test_22(){
+        hourlyNormalRate = new BigDecimal(5.0);
+        hourlyDiscountRate = new BigDecimal(2.0);
+        discountedPeriods.add(new Period(4,8));
+        normalPeriods.add(new Period(12,19));
+        normalPeriods.add(new Period(18, 22));
+
+        rate = new Rate(kind, hourlyNormalRate, hourlyDiscountRate, discountedPeriods, normalPeriods);
     }
 }
